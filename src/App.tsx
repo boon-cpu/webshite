1c28a63df9168b7ce772467871fb3982061c48ab
Boon <harrisonboon@gmail.com>
2021-01-07 23:43:51 +0000

    added about me :hankey:

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import twitter from "./img/twitter-fill.svg";
 import discord from "./img/discord-fill.svg";
 import github from "./img/github-fill.svg";
 import man from "./img/photo-1552058544-f2b08422138a.png";
-function App() {
+
+const App = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="App">
-      <div className="box">
+      <div id="drop" className={(open ? "open" : "") + " bar"}>
+        <div className="header">
+          <button
+            id="^"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            ^
+          </button>
+        </div>
+        <p id="waffle">
+          Hi there. I am a 15 year old full-stack TypeScript developer currently
+          based in the UK 🇬🇧. I am always looking to improve myself and the
+          world around us in any way possible. Using the latest technologies, I
+          can create software that is future-proof, efficient and scalable. Get
+          in touch on one of my socials!
+        </p>
+      </div>
+      <div className={"box " + (open ? "blur" : "")}>
         <div className="avatar">
           <img src={man} alt="avatar" className="avatar" />
         </div>
         <h1>Harrison Boon</h1>
         <p>Full-stack TypeScript engineer</p>
         <div className="about">
-          <button>About me</button>
+          <button
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            About me
+          </button>
           <div className="line" />
         </div>
         <div className="links">
@@ -37,6 +64,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
