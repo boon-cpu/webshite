4cd8cb3284383edaaeb938640052c7e46421e695
Boon <harrisonboon@gmail.com>
2021-01-07 20:47:40 +0000

    Innit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import twitter from "./img/twitter-fill.svg";
+import discord from "./img/discord-fill.svg";
+import github from "./img/github-fill.svg";
+import man from "./img/photo-1552058544-f2b08422138a.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <div className="avatar">
+          <img src={man} alt="avatar" className="avatar" />
+        </div>
+        <h1>Harrison Boon</h1>
+        <p>Full-stack TypeScript engineer</p>
+        <div className="about">
+          <button>About me</button>
+          <div className="line" />
+        </div>
+        <div className="links">
+          <div className="link-button">
+            <a href="https://twitter.com/boon_cpu">
+              <img src={twitter} alt="twitter" />
+            </a>
+          </div>
+          <div className="link-button">
+            <a href="https://dsc.bio/boon">
+              <img src={discord} alt="discord" />
+            </a>
+          </div>
+          <div className="link-button">
+            <a href="https://github.com/boon-cpu">
+              <img src={github} alt="github" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
